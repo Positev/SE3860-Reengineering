c86c08592a63860c55f646b8b458bbc03358a05b
liuzxjason <56461125+liuzxjason@users.noreply.github.com>
2021-02-21 11:56:07 -0600

    Tested ScoreKeeper

--- src/ScoreKeeper.py
+++ src/ScoreKeeper.py
@@ -1,7 +1,11 @@
-class Score:
+class ScoreKeeper:
 
-    def __init__(self, init_sorce = 0):
-        self.__score_dict = {0: init_sorce, 1: init_sorce}
+    def __init__(self, player_1_init_sorce=0, player_2_init_sorce=0):
+        self.__score_dict = {0: player_1_init_sorce, 1: player_2_init_sorce}
+
+    @property
+    def score_dict(self):
+        return self.__score_dict
 
     def record_win(self, player_id):
         if player_id in self.__score_dict:
@@ -9,6 +13,5 @@ class Score:
         else:
             raise Exception("Input a bwrong player_id.")
 
-    def get_score(player_id):
+    def get_score(self, player_id):
         return self.__score_dict[player_id]
-
