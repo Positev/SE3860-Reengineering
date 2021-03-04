5b8811523ff0bb9c779fbaec5330c4432820efbd
huberad <adamrhuber@gmail.com>
2021-03-04 12:52:38 -0600

    Added the score, velocity, and angle labels to gameScreen.py. Added velocity and angle edit boxes to gameScreen.py. SEMS21T04-30 SEMS21T04-32

--- Gorillas/ui/model/elements/text_box.py
+++ Gorillas/ui/model/elements/text_box.py
@@ -18,3 +18,10 @@ class TextBox(pygame.sprite.Sprite):
         self.rect = self.image.get_rect()
         self.rect.x = pos[0]
         self.rect.y = pos[1]
+
+    def update(self):
+        """Recreate the text image"""
+        self.image.fill(self.back_ground_color)
+        text_render = self.font.render(self.text, True, self.text_color)
+        text_render.get_rect().center = (self.image.get_size()[0] // 2, self.image.get_size()[1] // 2)
+        self.image.blit(text_render, text_render.get_rect(center=self.image.get_rect().center))
--- Gorillas/ui/model/gameScreen.py
+++ Gorillas/ui/model/gameScreen.py
@@ -1,9 +1,14 @@
 import pygame
-from model import Model
-from color import Color
-from elements.sprites.sun import Sun
-from elements.sprites.gorilla import Gorilla
-from Backend.Controllers.GameController import GameController
+from pygame.font import Font
+
+from Gorillas.ui.model.model import Model
+from Gorillas.color import Color
+from Gorillas.ui.model.elements.sprites.sun import Sun
+from Gorillas.ui.model.elements.sprites.gorilla import Gorilla
+from Gorillas.Backend.Controllers.GameController import GameController
+from Gorillas.ui.model.elements.text_box import TextBox
+from Gorillas.ui.model.elements.edit_box import EditBox
+
 
 
 class GameScreenModel(Model):
@@ -39,6 +44,45 @@ class GameScreenModel(Model):
         """Implement the Input UI and Score UI when Adam has them ready
             Here's more space 
         """
+        # Score UI
+
+        ui_font = pygame.font.Font(pygame.font.get_default_font(), 24)
+
+        scoreSize = pygame.font.Font.size(ui_font, "Score: 000")
+        player1_score_pos = (0, 0)
+        self.player1ScoreBox = TextBox(ui_font, scoreSize, player1_score_pos, text="Score: 0")
+        player2_score_pos = (screen_size[0] - scoreSize[0], 0)
+        self.player2ScoreBox = TextBox(ui_font, scoreSize, player2_score_pos, text="Score: 0")
+
+        self.render[0].add(self.player1ScoreBox)
+        self.render[0].add(self.player2ScoreBox)
+
+        angle_text = "Angle: "
+        velocity_text = "Velocity: "
+        angle_label_size = pygame.font.Font.size(ui_font, angle_text)
+        velocity_label_size = pygame.font.Font.size(ui_font, velocity_text)
+        edit_box_size = pygame.font.Font.size(ui_font, "00")
+        self.angle_label_positions = (
+            (0, player1_score_pos[1] + angle_label_size[1]),
+            (screen_size[0] - angle_label_size[0] - edit_box_size[0], player2_score_pos[1] + angle_label_size[1]))
+        self.velocity_label_positions = (
+            (0, self.angle_label_positions[0][1] + velocity_label_size[1]),
+            (screen_size[0] - velocity_label_size[0] - edit_box_size[0], self.angle_label_positions[1][1] + velocity_label_size[1]))
+        self.angle_edit_box_positions = (
+            (self.angle_label_positions[0][0] + angle_label_size[0], self.angle_label_positions[0][0]),
+            (self.angle_label_positions[1][0] + angle_label_size[0], self.angle_label_positions[1][1]))
+        self.velocity_edit_box_positions = (
+            (self.velocity_label_positions[0][0] + velocity_label_size[0], self.velocity_label_positions[0][1]),
+            (self.velocity_label_positions[1][0] + velocity_label_size[0], self.velocity_label_positions[1][1]))
+
+        self.angle_label = \
+            TextBox(ui_font, angle_label_size, self.angle_label_positions[1], text=angle_text)
+        self.velocity_label = \
+            TextBox(ui_font, velocity_label_size, self.velocity_label_positions[1], text=velocity_text)
+
+        self.angle_edit_box = EditBox(ui_font, edit_box_size, self.angle_edit_box_positions[1], back_ground_color=Color.LIGHT_GRAY)
+        self.velocity_edit_box = EditBox(ui_font, edit_box_size, self.velocity_edit_box_positions[1], back_ground_color=Color.LIGHT_GRAY)
+
         # Create the wind arrow
         self.arrow_image = pygame.image.load("../../../../Sprites/arrow.png")
         wind_width = game_state.wind.velocity * self.WIND_DEFAULT_WIDTH
@@ -69,4 +113,3 @@ class GameScreenModel(Model):
 
     def update_frame(self, frame):
         """Updates the background to be a new frame of the game"""
-
