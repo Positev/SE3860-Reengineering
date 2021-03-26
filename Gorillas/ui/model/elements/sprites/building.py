3df40e3eee5437c525d9482b78344ed9ba399542
Robert Border <borderr@uwplatt.edu>
2021-03-26 13:11:56 -0500

    Removed unnecessary method

--- Gorillas/ui/model/elements/sprites/building.py
+++ Gorillas/ui/model/elements/sprites/building.py
@@ -13,21 +13,5 @@ class Building(pygame.sprite.Sprite):
         self.size = size
         self.pos = pos
 
-    def create_windows(self):
-        # Let's see if making the windows this way is a possibility
-        num_horizontal = random.randint(3, 7)
-        num_vertical = random.randint(4, 8)
-        window_size = [self.size[0] / num_horizontal, self.size[1] / num_vertical]
-        window_pos = [self.pos[0] + window_size[0], self.pos[1] + window_size[1]]
-        max_x = self.pos[0] + self.size[0]
-        max_y = self.pos[1] + self.size[1]
 
-        while window_pos[1] < max_y:
-            while window_pos[0] < max_x:
-                window_rect = (window_pos[0], window_pos[1], window_size[0], window_size[1])
-                pygame.draw.rect(self.image, self.WINDOW_COLOR, window_rect)
-                window_pos[0] += window_size[0] * 2
-
-            window_pos[0] = self.pos[0] + window_size[0]
-            window_pos[1] += window_size[1] * 2
 
