3b71763cf5d4f8b333efc184141514233abe8dd1
Robert Border <borderr@uwplatt.edu>
2021-03-26 10:45:30 -0500

    Error fixing for window generation

--- Gorillas/ui/model/elements/sprites/building.py
+++ Gorillas/ui/model/elements/sprites/building.py
@@ -1,5 +1,5 @@
 import pygame
-
+import random
 
 class Building(pygame.sprite.Sprite):
     WINDOW_COLOR = (192, 192, 192)
@@ -17,15 +17,15 @@ class Building(pygame.sprite.Sprite):
         # Let's see if making the windows this way is a possibility
         num_horizontal = random.randint(3, 7)
         num_vertical = random.randint(4, 8)
-        window_size = (self.size[0] / num_horizontal, self.size[1] / num_vertical)
-        window_pos = (self.pos[0] + window_size[0], self.pos[1] + window_size[1])
+        window_size = [self.size[0] / num_horizontal, self.size[1] / num_vertical]
+        window_pos = [self.pos[0] + window_size[0], self.pos[1] + window_size[1]]
         max_x = self.pos[0] + self.size[0]
         max_y = self.pos[1] + self.size[1]
 
         while window_pos[1] < max_y:
             while window_pos[0] < max_x:
                 window_rect = (window_pos[0], window_pos[1], window_size[0], window_size[1])
-                pygame.draw.rect(self.image, WINDOW_COLOR, window_rect)
+                pygame.draw.rect(self.image, self.WINDOW_COLOR, window_rect)
                 window_pos[0] += window_size[0] * 2
 
             window_pos[0] = self.pos[0] + window_size[0]
